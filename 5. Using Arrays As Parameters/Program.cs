8be3e76ef5f1e6102367311c7947820bcd9f2837
Marcos Andrade <mvpdeandrade@gmail.com>
2023-02-09 17:12:46 -0300

    Adiciona outro método para treinar o uso de arrays como parâmetros

--- 5. Using Arrays As Parameters/Program.cs
+++ 5. Using Arrays As Parameters/Program.cs
@@ -6,12 +6,35 @@ namespace _5._Using_Arrays_As_Parameters
     {
         static void Main(string[] args)
         {
-            int[] studentsGrades = new int[] { 15, 13, 8, 12, 6 };
+            int[] studentsGrades = new int[] { 15, 13, 8, 12, 6, 16 };
             double averageResult = GetAverage(studentsGrades);
+
+
+            int[] happiness = new int[] { 1, 2, 3, 4, 5 };
+            SunIsShining(happiness);
+
+            foreach (int grade in studentsGrades)
+            {
+                Console.WriteLine("{0}", grade);
+            }
             Console.WriteLine("The average is: {0}", averageResult);
             Console.ReadKey();
+
+
+            foreach (int felicidad in happiness)
+            {
+                Console.WriteLine(felicidad);
+            }
+            Console.ReadLine();
         }
 
+        static void SunIsShining(int[] happiness)
+        {
+            for (int i = 0; i < happiness.Length; i++)
+            {
+                happiness[i] += 2;
+            }
+        }
 
         static double GetAverage(int[] gradesArray)
         {
