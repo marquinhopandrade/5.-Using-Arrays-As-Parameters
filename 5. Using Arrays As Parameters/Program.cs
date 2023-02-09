f32093a0d88cbbf95302445c546bdd11203e08bc
Marcos Andrade <mvpdeandrade@gmail.com>
2023-02-09 16:31:17 -0300

    Adicionar arquivos de projeto.

---
+++ 5. Using Arrays As Parameters/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _5._Using_Arrays_As_Parameters
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] studentsGrades = new int[] { 15, 13, 8, 12, 6 };
+            double averageResult = GetAverage(studentsGrades);
+            Console.WriteLine("The average is: {0}", averageResult);
+        }
+
+        static double GetAverage(int[] gradesArray)
+        {
+            int size = gradesArray.Length;
+            double average;
+            int sum = 0;
+            for (int i = 0; i < size; i++)
+            {
+                sum += gradesArray[i];
+            }
+            average = (double)sum / size;
+            return average;
+        }
+    }
+}
